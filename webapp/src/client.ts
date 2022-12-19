1868fd795a9795486b640c7c2f3e05521d029bc0
maTORIx <matorixmatrix@gmail.com>
2022-12-19 16:20:52 +0900

    Fix connect client url bug

--- webapp/src/client.ts
+++ webapp/src/client.ts
@@ -7,7 +7,7 @@ import { ChatService } from "./gen/chat/v1/chat_connectweb";
 const baseUrl = location.host === "localhost:5173" ? "http://localhost:8080" : location.origin
 
 const transport = createConnectTransport({
-  baseUrl: "http://localhost:8080",
+  baseUrl: baseUrl,
 });
 
 export const client = createPromiseClient(ChatService, transport);
